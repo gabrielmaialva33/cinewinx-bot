63626f8871b81f2577adee32f73741ce3e714a74
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-06-29 13:51:08 -0300

    chore: configure py project

---
+++ config.py
@@ -0,0 +1,15 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get this value from my.telegram.org/apps
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
