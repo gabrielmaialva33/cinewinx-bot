fa53fd74532cac6896e3dc0f5c63f0a8058b0724
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-07-20 18:54:23 -0300

    refactor: bot plugins

--- CineWinx/platforms/Youtube.py
+++ CineWinx/platforms/Youtube.py
@@ -306,7 +306,7 @@ class YouTubeAPI:
                     {
                         "key": "FFmpegExtractAudio",
                         "preferredcodec": "mp3",
-                        "preferredquality": "192",
+                        "preferredquality": "320",
                     }
                 ],
             }
