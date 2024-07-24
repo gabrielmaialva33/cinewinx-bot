ec5a32b7cfbf3596fb1cd7d2033fa9dfc8cc4c9e
Maia <gabrielmaialva33@gmail.com>
2024-07-24 02:30:02 +0000

    chore: auto fixes

--- CineWinx/plugins/extra/password.py
+++ CineWinx/plugins/extra/password.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from CineWinx import app
 
 
-@app.on_message(filters.command(["genpassword", 'genpw']))
+@app.on_message(filters.command(["genpassword", "genpw"]))
 async def password(_client: Client, message: Message):
     reply_message = await message.reply_text(text="⏳ 𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗮𝗻𝗱𝗼..")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
@@ -19,7 +19,19 @@ async def password(_client: Client, message: Message):
 
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
-    txt = f"<b>🔢 𝗟𝗶𝗺𝗶𝘁𝗲:</b> {str(limit)} \n<b>🔐 𝗦𝗲𝗻𝗵𝗮:</b> <code>{random_value}</code>"
+    txt = (
+        f"<b>🔢 𝗟𝗶𝗺𝗶𝘁𝗲:</b> {str(limit)} \n<b>🔐 𝗦𝗲𝗻𝗵𝗮:</b> <code>{random_value}</code>"
+    )
     me = await app.get_me()
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('➕ 𝗔𝗗𝗜𝗖𝗜𝗢𝗡𝗔𝗥', url=f'https://t.me/{me.username}?startgroup=true')]])
-    await reply_message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
+    btn = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "➕ 𝗔𝗗𝗜𝗖𝗜𝗢𝗡𝗔𝗥", url=f"https://t.me/{me.username}?startgroup=true"
+                )
+            ]
+        ]
+    )
+    await reply_message.edit_text(
+        text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML
+    )
