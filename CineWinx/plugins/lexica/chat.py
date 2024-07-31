411bfeed638c89982a05f9d529b5101b98a8da5d
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-07-31 13:14:56 -0300

    feat: implement lexica api

---
+++ CineWinx/plugins/lexica/chat.py
@@ -0,0 +1,70 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+from CineWinx import app
+from CineWinx.utils import get_chats_model
+from config import PREFIXES, BANNED_USERS
+from strings import get_command
+
+LLM_COMMAND = get_command("LLM_COMMAND")
+
+prompt_db = {}
+
+MODELS = {}
+
+
+@app.on_message(
+    filters.command(LLM_COMMAND, PREFIXES) & filters.group & ~BANNED_USERS
+)
+async def llm(_client: Client, message: Message):
+    models = await get_chats_model()
+
+    page = 0
+    markup = chat_markup(models, page)
+
+    await message.reply_text(
+        "📝 𝗘𝗹𝗶𝗴𝗲 𝗼 𝗺𝗼𝗱𝗲𝗹𝗼 𝗾𝘂𝗲 𝗱𝗲𝘀𝗲𝗮𝘀 𝗰𝗼𝗻𝘀𝘂𝗹𝘁𝗮𝗿.",
+        reply_markup=markup,
+    )
+
+
+# paginate models into 4 per row with next
+def chat_markup(models: list, page: int):
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=model["name"],
+                    callback_data=f"llm_{model['id']}",
+                )
+                for model in models[page * 4: page * 4 + 4]
+            ],
+            [
+                InlineKeyboardButton(
+                    "« 𝗔𝗻𝘁𝗲𝗿𝗶𝗼𝗿",
+                    callback_data=f"llm_prev_{page}",
+                ),
+                InlineKeyboardButton(
+                    "𝗦𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲 »",
+                    callback_data=f"llm_next_{page}",
+                ),
+            ],
+        ]
+    )
+
+
+# Callbacks pagination
+@app.on_callback_query(filters.regex(pattern=r"^llm_(prev|next)_\d+"))
+async def paginate_models(_, callback_query):
+    models = await get_chats_model()
+    data = callback_query.data.split("_")
+    page = int(data[2])
+
+    if data[1] == "prev":
+        page -= 1
+    elif data[1] == "next":
+        page += 1
+
+    markup = chat_markup(models, page)
+
+    await callback_query.edit_message_reply_markup(markup)
--- CineWinx/utils/lexica.py
+++ CineWinx/utils/lexica.py
@@ -1,238 +1,128 @@
-from CineWinx.utils.httpx import fetch
+import asyncio
+from pyrogram.types import InlineKeyboardButton
 
-API_URL = "https://lexica.qewertyy.me"
-
-
-async def list_models():
-    return await fetch.get(f"{API_URL}/models")
+from lexica import AsyncClient, languageModels
 
 
-# {
-#     "models": {
-#         "chat": [
-#             {
-#                 "id": 1,
-#                 "name": "Gemma",
-#                 "baseModel": ""
-#             },
-#             {
-#                 "id": 5,
-#                 "name": "gpt-3.5-turbo",
-#                 "baseModel": "GPT"
-#             },
-#             {
-#                 "id": 19,
-#                 "name": "gpt-4",
-#                 "baseModel": "GPT",
-#                 "type": "Premium"
-#             },
-#             {
-#                 "id": 18,
-#                 "name": "Llama 2",
-#                 "baseModel": "Llama",
-#                 "version": "7b-chat-int8"
-#             },
-#             {
-#                 "id": 14,
-#                 "name": "Llama 2",
-#                 "baseModel": "Llama",
-#                 "version": "7b-chat-fp16"
-#             },
-#             {
-#                 "id": 20,
-#                 "name": "bard",
-#                 "baseModel": "LLM"
-#             },
-#             {
-#                 "id": 21,
-#                 "name": "Mistral",
-#                 "baseModel": "LLM",
-#                 "version": "7b-instruct-v0.1"
-#             },
-#             {
-#                 "id": 23,
-#                 "name": "Gemini-Pro",
-#                 "baseModel": "PaLM 2"
-#             },
-#             {
-#                 "id": 24,
-#                 "name": "Gemini-Pro-Vision",
-#                 "baseModel": "PaLM 2"
-#             },
-#             {
-#                 "id": 26,
-#                 "name": "Llama 2",
-#                 "baseModel": "Llama",
-#                 "version": "13b-chat-awq"
-#             },
-#             {
-#                 "id": 27,
-#                 "name": "OpenHermes",
-#                 "baseModel": "Mistral 7B",
-#                 "version": "2.5-mistral-7b-awq"
-#             },
-#             {
-#                 "id": 31,
-#                 "name": "Zephyr",
-#                 "baseModel": "LLM",
-#                 "version": "7b-beta-awq"
-#             }
-#         ],
-#         "image": [
-#             {
-#                 "id": 2,
-#                 "name": "Meina Mix",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 3,
-#                 "name": "AnyLora",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 4,
-#                 "name": "AnyThingV4",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 7,
-#                 "name": "Goofball Mix",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 8,
-#                 "name": "MeinaHentai",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 9,
-#                 "name": "DarkSushi Mix",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 10,
-#                 "name": "SDXL",
-#                 "baseModel": "SDXL 1.0"
-#             },
-#             {
-#                 "id": 11,
-#                 "name": "Creative",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 12,
-#                 "name": "CreativeV2",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 13,
-#                 "name": "Absolute Reality",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 17,
-#                 "name": "CalicoMix",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 15,
-#                 "name": "AnimeV1",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 16,
-#                 "name": "Lexica",
-#                 "baseModel": "SD",
-#                 "type": "Exclusive"
-#             },
-#             {
-#                 "id": 34,
-#                 "name": "Dreamshaper",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 25,
-#                 "name": "CetusMix",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 30,
-#                 "name": "Anime",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 32,
-#                 "name": "Anime 2",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 33,
-#                 "name": "Dall-E",
-#                 "baseModel": "Dall-E"
-#             },
-#             {
-#                 "id": 35,
-#                 "name": "Realistic",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 36,
-#                 "name": "Logo",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 39,
-#                 "name": "Sticker",
-#                 "baseModel": "SD"
-#             },
-#             {
-#                 "id": 40,
-#                 "name": "Anime v1.1",
-#                 "baseModel": "SD"
-#             }
-#         ],
-#         "AntiNSFW": [
-#             {
-#                 "id": 28,
-#                 "name": "Serene"
-#             },
-#             {
-#                 "id": 29,
-#                 "name": "nsfwjs",
-#                 "source": "https://github.com/infinitered/nsfwjs"
-#             },
-#             {
-#                 "id": 41,
-#                 "name": "Limpid?"
-#             }
-#         ],
-#         "customGPTs": [
-#             {
-#                 "id": 22,
-#                 "name": "Title Generator",
-#                 "baseModel": "gpt-3.5-turbo"
-#             }
-#         ],
-#         "upscale": [
-#             {
-#                 "name": "Lexica",
-#                 "id": 37
-#             },
-#             {
-#                 "name": "Pixel",
-#                 "id": 38
-#             }
-#         ]
-#     },
-#     "message": "ok",
-#     "code": 2
-# }
+async def list_models() -> dict:
+    models = await AsyncClient().getModels()
+    print(models, "models")
+    ### Output:
+    # {
+    #   "models": {
+    #     "chat": [
+    #       { "id": 1, "name": "Gemma", "baseModel": "" },
+    #       { "id": 5, "name": "gpt-3.5-turbo", "baseModel": "GPT" },
+    #       { "id": 19, "name": "gpt-4", "baseModel": "GPT", "type": "Premium" },
+    #       {
+    #         "id": 18,
+    #         "name": "Llama 2",
+    #         "baseModel": "Llama",
+    #         "version": "7b-chat-int8"
+    #       },
+    #       {
+    #         "id": 14,
+    #         "name": "Llama 2",
+    #         "baseModel": "Llama",
+    #         "version": "7b-chat-fp16"
+    #       },
+    #       { "id": 20, "name": "bard", "baseModel": "LLM" },
+    #       {
+    #         "id": 21,
+    #         "name": "Mistral",
+    #         "baseModel": "LLM",
+    #         "version": "7b-instruct-v0.1"
+    #       },
+    #       { "id": 23, "name": "Gemini-Pro", "baseModel": "PaLM 2" },
+    #       { "id": 24, "name": "Gemini-Pro-Vision", "baseModel": "PaLM 2" },
+    #       {
+    #         "id": 26,
+    #         "name": "Llama 2",
+    #         "baseModel": "Llama",
+    #         "version": "13b-chat-awq"
+    #       },
+    #       {
+    #         "id": 27,
+    #         "name": "OpenHermes",
+    #         "baseModel": "Mistral 7B",
+    #         "version": "2.5-mistral-7b-awq"
+    #       },
+    #       {
+    #         "id": 31,
+    #         "name": "Zephyr",
+    #         "baseModel": "LLM",
+    #         "version": "7b-beta-awq"
+    #       }
+    #     ],
+    #     "image": [
+    #       { "id": 2, "name": "Meina Mix", "baseModel": "SD" },
+    #       { "id": 3, "name": "AnyLora", "baseModel": "SD" },
+    #       { "id": 4, "name": "AnyThingV4", "baseModel": "SD" },
+    #       { "id": 7, "name": "Goofball Mix", "baseModel": "SD" },
+    #       { "id": 8, "name": "MeinaHentai", "baseModel": "SD" },
+    #       { "id": 9, "name": "DarkSushi Mix", "baseModel": "SD" },
+    #       { "id": 10, "name": "SDXL", "baseModel": "SDXL 1.0" },
+    #       { "id": 11, "name": "Creative", "baseModel": "SD" },
+    #       { "id": 12, "name": "CreativeV2", "baseModel": "SD" },
+    #       { "id": 13, "name": "Absolute Reality", "baseModel": "SD" },
+    #       { "id": 17, "name": "CalicoMix", "baseModel": "SD" },
+    #       { "id": 15, "name": "AnimeV1", "baseModel": "SD" },
+    #       { "id": 16, "name": "Lexica", "baseModel": "SD", "type": "Exclusive" },
+    #       { "id": 34, "name": "Dreamshaper", "baseModel": "SD" },
+    #       { "id": 25, "name": "CetusMix", "baseModel": "SD" },
+    #       { "id": 30, "name": "Anime", "baseModel": "SD" },
+    #       { "id": 32, "name": "Anime 2", "baseModel": "SD" },
+    #       { "id": 33, "name": "Dall-E", "baseModel": "Dall-E" },
+    #       { "id": 35, "name": "Realistic", "baseModel": "SD" },
+    #       { "id": 36, "name": "Logo", "baseModel": "SD" },
+    #       { "id": 39, "name": "Sticker", "baseModel": "SD" },
+    #       { "id": 40, "name": "Anime v1.1", "baseModel": "SD" }
+    #     ],
+    #     "AntiNSFW": [
+    #       { "id": 28, "name": "Serene" },
+    #       {
+    #         "id": 29,
+    #         "name": "nsfwjs",
+    #         "source": "https://github.com/infinitered/nsfwjs"
+    #       },
+    #       { "id": 41, "name": "Limpid?" }
+    #     ],
+    #     "customGPTs": [
+    #       { "id": 22, "name": "Title Generator", "baseModel": "gpt-3.5-turbo" }
+    #     ],
+    #     "upscale": [
+    #       { "name": "Lexica", "id": 37 },
+    #       { "name": "Pixel", "id": 38 }
+    #     ]
+    #   },
+    #   "message": "ok",
+    #   "code": 2
+    # }
+    return models
+
 
 async def get_chats_model():
     response = await list_models()
+
+
     return response["models"]["chat"]
 
 
 async def get_images_model():
     response = await list_models()
     return response["models"]["image"]
+
+
+async def get_anti_nsfw_model():
+    response = await list_models()
+    return response["models"]["AntiNSFW"]
+
+
+async def get_custom_gpts_model():
+    response = await list_models()
+    return response["models"]["customGPTs"]
+
+
+async def get_upscale_model():
+    response = await list_models()
+    return response["models"]["upscale"]
