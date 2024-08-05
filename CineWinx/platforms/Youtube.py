4a769a9a6d9ac80ad4e7190f848ffe8d4351feb1
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-08-04 23:52:08 -0300

    refactor: yt platforms

--- CineWinx/platforms/Youtube.py
+++ CineWinx/platforms/Youtube.py
@@ -66,7 +66,7 @@ class YouTubeAPI:
                         return entity.url
         if offset in (None,):
             return None
-        return text[offset : offset + length]
+        return text[offset: offset + length]
 
     async def details(self, link: str, videoid: Union[bool, str] = None):
         title = None
@@ -80,7 +80,7 @@ class YouTubeAPI:
             link = link.split("&")[0]
         if "?" in link:
             link = link.split("?")[0]
-        results = VideosSearch(link, limit=1)
+        results = VideosSearch(link, limit=1, language="pt-BR", region="BR")
         for result in (await results.next())["result"]:
             title = result["title"]
             duration_min = result["duration"]
@@ -217,10 +217,10 @@ class YouTubeAPI:
         return formats_available, link
 
     async def slider(
-        self,
-        link: str,
-        query_type: int,
-        videoid: Union[bool, str] = None,
+            self,
+            link: str,
+            query_type: int,
+            videoid: Union[bool, str] = None,
     ):
         if videoid:
             link = self.base + link
@@ -235,15 +235,15 @@ class YouTubeAPI:
         return title, duration_min, thumbnail, vidid
 
     async def download(
-        self,
-        link: str,
-        mystic,
-        video: Union[bool, str] = None,
-        videoid: Union[bool, str] = None,
-        songaudio: Union[bool, str] = None,
-        songvideo: Union[bool, str] = None,
-        format_id: Union[bool, str] = None,
-        title: Union[bool, str] = None,
+            self,
+            link: str,
+            mystic,
+            video: Union[bool, str] = None,
+            videoid: Union[bool, str] = None,
+            songaudio: Union[bool, str] = None,
+            songvideo: Union[bool, str] = None,
+            format_id: Union[bool, str] = None,
+            title: Union[bool, str] = None,
     ) -> str:
         if videoid:
             link = self.base + link
