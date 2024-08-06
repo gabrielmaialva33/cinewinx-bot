ebacabb5417d6201e7cd0beae1e1b6f774b60760
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-08-06 20:03:54 -0300

    feat: implement ip commad

--- CineWinx/core/userbot.py
+++ CineWinx/core/userbot.py
@@ -181,21 +181,21 @@ class Userbot(Client):
                 )
                 await self.one.send_message(chat_id=config.LOG_GROUP_ID, text=text)
 
-                if WINX_ECOSYSTEM_IDS and IN_DEV_MODE == str(False):
-                    me = await self.one.get_me()
-                    pic = (
-                        await self.one.download_media(me.photo.big_file_id)
-                        if me.photo
-                        else None
-                    )
-
-                    for chat in WINX_ECOSYSTEM_IDS:
-                        await self.one.join_chat(chat)
-
-                        if pic:
-                            await self.one.send_photo(
-                                chat_id=chat, photo=pic, caption=text
-                            )
+                # if WINX_ECOSYSTEM_IDS and IN_DEV_MODE == str(False):
+                #     me = await self.one.get_me()
+                #     pic = (
+                #         await self.one.download_media(me.photo.big_file_id)
+                #         if me.photo
+                #         else None
+                #     )
+                #
+                #     for chat in WINX_ECOSYSTEM_IDS:
+                #         await self.one.join_chat(chat)
+                #
+                #         if pic:
+                #             await self.one.send_photo(
+                #                 chat_id=chat, photo=pic, caption=text
+                #             )
 
             except (errors.ChannelInvalid, errors.PeerIdInvalid):
                 LOGGER(__name__).error("LOGGER_GROUP_ID is invalid.")
