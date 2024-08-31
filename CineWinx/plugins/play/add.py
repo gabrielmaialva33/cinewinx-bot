8ecc521cb4acac03a2c7addb622cb91d86e1752e
Maia <gabrielmaialva33@gmail.com>
2024-08-31 21:28:58 +0000

    chore: auto fixes

--- CineWinx/plugins/play/add.py
+++ CineWinx/plugins/play/add.py
@@ -7,8 +7,7 @@ from strings import get_command
 
 ADD_COMMAND = get_command("ADD_COMMAND")
 
-@app.on_message(
-    filters.command(ADD_COMMAND, PREFIXES) & filters.group & ~BANNED_USERS
-)
+
+@app.on_message(filters.command(ADD_COMMAND, PREFIXES) & filters.group & ~BANNED_USERS)
 async def add_command(client: Client, message: Message):
     pass
