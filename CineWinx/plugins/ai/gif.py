64d421c7f2f2b2f8d982f2f872634153e816dcef
Maia <gabrielmaialva33@gmail.com>
2024-09-03 05:17:51 +0000

    chore: auto fixes

--- CineWinx/plugins/ai/gif.py
+++ CineWinx/plugins/ai/gif.py
@@ -11,13 +11,13 @@ from config import PREFIXES, BANNED_USERS
 
 SPACE_URL = "https://guardiancc-flux-gif-animations-2.hf.space/call/infer"
 
+
 @app.on_message(filters.command(["gif"], PREFIXES) & ~BANNED_USERS)
 async def hf_gif(_client: Client, message: Message):
     try:
         text = message.text.split(" ", 1)[1]
     except IndexError:
-        await message.reply_text(
-            "📝 𝗩𝗼𝗰𝗲 𝗽𝗼𝗱𝗲 𝗲𝗻𝘃𝗶𝗮𝗿 𝗼 𝘁𝗲𝘅𝘁𝗼 𝗽𝗮𝗿𝗮 𝗰𝗼𝗻𝘃𝗲𝗿𝘁𝗲 𝗲𝗺 𝗚𝗜𝗙.")
+        await message.reply_text("📝 𝗩𝗼𝗰𝗲 𝗽𝗼𝗱𝗲 𝗲𝗻𝘃𝗶𝗮𝗿 𝗼 𝘁𝗲𝘅𝘁𝗼 𝗽𝗮𝗿𝗮 𝗰𝗼𝗻𝘃𝗲𝗿𝘁𝗲 𝗲𝗺 𝗚𝗜𝗙.")
         return
 
     async with aiohttp.ClientSession() as session:
@@ -38,19 +38,20 @@ async def hf_gif(_client: Client, message: Message):
             event_id = data["event_id"]
             logging.info(f"event ID: {event_id}")
 
-            async with session.get(
-                    f"{SPACE_URL}/{event_id}"
-            ) as event_resp:
+            async with session.get(f"{SPACE_URL}/{event_id}") as event_resp:
                 if event_resp.content_type == "text/event-stream":
                     async for line in event_resp.content:
-                        event_data = line.decode('utf-8').strip()
+                        event_data = line.decode("utf-8").strip()
                         if event_data.startswith("data:"):
                             json_str = event_data.replace("data:", "").strip()
                             try:
                                 json_data = json.loads(json_str)
                                 if json_data not in [None, ""]:
                                     await mystic.delete()
-                                    await message.reply_animation(json_data[0]["url"], caption=f"🎞️ 𝗚𝗜𝗙 𝗴𝗲𝗿𝗮𝗱𝗼 𝗰𝗼𝗺 𝗼 𝘁𝗲𝘅𝘁𝗼: <code>{text}</code>")
+                                    await message.reply_animation(
+                                        json_data[0]["url"],
+                                        caption=f"🎞️ 𝗚𝗜𝗙 𝗴𝗲𝗿𝗮𝗱𝗼 𝗰𝗼𝗺 𝗼 𝘁𝗲𝘅𝘁𝗼: <code>{text}</code>",
+                                    )
                             except json.JSONDecodeError:
                                 logging.error(f"error decoding json: {json_str}")
                                 await mystic.edit_text("🚫 𝗘𝗿𝗿𝗼𝗿 𝗮𝗼 𝗴𝗲𝗿𝗮𝗿 𝗼 𝗚𝗜𝗙.")
@@ -61,4 +62,3 @@ async def hf_gif(_client: Client, message: Message):
                     logging.error(f"unexpected content-type: {event_resp.content_type}")
                     await mystic.edit_text("🚫 𝗘𝗿𝗿𝗼𝗿 𝗮𝗼 𝗴𝗲𝗿𝗮𝗿 𝗼 𝗚𝗜𝗙.")
                     await session.close()
-
