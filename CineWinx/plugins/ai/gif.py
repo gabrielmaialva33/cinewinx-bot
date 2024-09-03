ccf97370b6e15d3bb10719ecd4411d53f9774e97
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-09-03 02:07:01 -0300

    feat: implement ai space

--- CineWinx/plugins/ai/gif.py
+++ CineWinx/plugins/ai/gif.py
@@ -1,30 +1,13 @@
-import asyncio
 import logging
 import json
-from typing import Union, Dict, Any, Optional, TypedDict, List
 
 import aiohttp
 
-from gradio_client import Client as GradioClient
-
 from pyrogram import filters, Client
-from pyrogram.errors import FloodWait
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import Message
 
 from CineWinx import app
-from CineWinx.utils.winx_font import Fonts
-from config import PREFIXES, BANNED_USERS, HF_TOKEN
-from strings import get_command
-
-# curl -X POST https://guardiancc-flux-gif-animations-2.hf.space/call/infer -s -H "Content-Type: application/json" -d '{
-#   "data": [
-# 							"Hello!!",
-# 							0,
-# 							true,
-# 							1
-# ]}' \
-#   | awk -F'"' '{ print $4}'  \
-#   | read EVENT_ID; curl -N https://guardiancc-flux-gif-animations-2.hf.space/call/infer/$EVENT_ID
+from config import PREFIXES, BANNED_USERS
 
 @app.on_message(filters.command(["gif"], PREFIXES) & ~BANNED_USERS)
 async def hf_gif(_client: Client, message: Message):
@@ -63,8 +46,6 @@ async def hf_gif(_client: Client, message: Message):
                             json_str = event_data.replace("data:", "").strip()
                             try:
                                 json_data = json.loads(json_str)
-                                print("json_data", json_data)
-                                #  [{'path': '/tmp/gradio/9a0eb62f04b8ba012618cb568bf8be2007181d793b8297bda622dd44df3b0c8f/9b4b56650fe4451d80ec2e95446636f6-flux.gif', 'url': 'https://guardiancc-flux-gif-animations-2.hf.space/file=/tmp/gradio/9a0eb62f04b8ba012618cb568bf8be2007181d793b8297bda622dd44df3b0c8f/9b4b56650fe4451d80ec2e95446636f6-flux.gif', 'size': None, 'orig_name': '9b4b56650fe4451d80ec2e95446636f6-flux.gif', 'mime_type': None, 'is_stream': False, 'meta': {'_type': 'gradio.FileData'}}, {'path': '/tmp/gradio/9f49a411b6fbe57827c57b7bf6501cbf3a9bc56dd36f548eb2c20755eebb37a2/image.webp', 'url': 'https://guardiancc-flux-gif-animations-2.hf.space/file=/tmp/gradio/9f49a411b6fbe57827c57b7bf6501cbf3a9bc56dd36f548eb2c20755eebb37a2/image.webp', 'size': None, 'orig_name': 'image.webp', 'mime_type': None, 'is_stream': False, 'meta': {'_type': 'gradio.FileData'}}, 907086999]
                                 if json_data not in [None, ""]:
                                     await mystic.delete()
                                     await message.reply_animation(json_data[0]["url"], caption=f"🎞️ 𝗚𝗜𝗙 𝗴𝗲𝗿𝗮𝗱𝗼 𝗰𝗼𝗺 𝗼 𝘁𝗲𝘅𝘁𝗼: <code>{text}</code>")
